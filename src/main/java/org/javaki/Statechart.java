3f0a35930358cdceafcbe5195be53bec95a234a2
Joachim Haagen Skeie <joachim@haagen.name>
2011-10-24 23:52:24 +0200

    Adding traversal of the statechart both up and down and between branches.

--- src/main/java/org/javaki/Statechart.java
+++ src/main/java/org/javaki/Statechart.java
@@ -1,6 +1,7 @@
 package org.javaki;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Statechart {
@@ -10,8 +11,8 @@ public class Statechart {
 	private State initialState = null;
 	private State currentState = null;
 	
-	private List<State> createStateChain = new ArrayList<State>();
-	private List<State> destroyStateChain = new ArrayList<State>();
+	List<State> createStateChain = new ArrayList<State>();
+	List<State> destroyStateChain = new ArrayList<State>();
 	
 	public Statechart(String name, State rootState) {
 		this.name = name;
@@ -31,11 +32,10 @@ public class Statechart {
 			statechartIsInitialized = true;
 			if (rootState == null) {
 				rootState = new State(null, "rootState", null, null);
-				initialState = rootState;
 			}
 			
+			initialState = rootState;
 			rootState.enterState();
-			
 			currentState = initialState;
 		}
 	}
@@ -44,6 +44,87 @@ public class Statechart {
 		createStateChain.clear();
 		destroyStateChain.clear();
 		
-		return rootState.getSubstate(stateName);
+		if (currentState.getName().equals(stateName)) {
+			//No transitions, return currentState
+			return currentState;
+		}
+		
+		State substateOfCurrent = currentState.getSubstate(stateName);
+		State parentOfCurrent = currentState.getParentState(stateName);
+		State differentBranchState = rootState.getSubstate(stateName);
+		
+		if (substateOfCurrent != null) {
+			//stateName is a substate of currentState, no destroy needed
+			
+			//Go to the targetState (substateOfCurrent), and traverse back upwards the parent chain until the current node is reached. 
+			List<State> reversedOrderStates = getNodesFromSourceToParent(stateName, substateOfCurrent, currentState);
+			Collections.reverse(reversedOrderStates);
+			createStateChain.addAll(reversedOrderStates);
+			currentState = substateOfCurrent;
+		} else if (parentOfCurrent != null) {
+			//stateName is a parent of currentState, no create needed
+			//Add path from current to parentOfCurrent destroyStateChain
+			destroyStateChain.addAll(getNodesFromSourceToParent(stateName, currentState, parentOfCurrent));
+			currentState = parentOfCurrent;
+		} else if (differentBranchState != null) {
+			//stateName is part of the tree, but not in the current branch.
+			//Get path from current to root
+			traverseBetweenTreeBranches(stateName, differentBranchState);
+			//find first common root node
+			//TODO: Add path to first common root node to destroyStateChain
+			//TODO: Add path from first common root node to stateName to createStateChain
+		} else {
+			return null;
+		}
+		
+		
+		return currentState;
+	}
+
+	private void traverseBetweenTreeBranches(String stateName,
+			State differentBranchState) {
+		List<State> currentToRootPath = getNodesFromSourceToParent(rootState.getName(), currentState, rootState);
+		//Get path from target to root
+		List<State> targetToRootPath = getNodesFromSourceToParent(rootState.getName(), differentBranchState, rootState);
+		
+		State commonRootState = null;
+		for (State fromBranchState : currentToRootPath) {
+			for (State toBranchState : targetToRootPath) {
+				if (fromBranchState.getName().equals(toBranchState.getName())) {
+					//We found our first common root node
+					commonRootState = fromBranchState;
+					break;
+				} 
+			}
+			
+			//Add fromBranchState to destroyStateChain
+			if (commonRootState != null) {
+				break;
+			}
+			destroyStateChain.add(fromBranchState);
+		}
+		
+		if (commonRootState == null) {
+			commonRootState = rootState;
+		}
+		List<State> reversedOrderStates = getNodesFromSourceToParent(stateName, differentBranchState, commonRootState);
+		Collections.reverse(reversedOrderStates);
+		createStateChain.addAll(reversedOrderStates);
+		currentState = differentBranchState;
+	}
+
+	private List<State> getNodesFromSourceToParent(String stateName, State sourceState, State targetState) {
+		List<State> statesTraversed = new ArrayList<State>();
+		State currState = sourceState;
+		while (currState!= null) {
+			if (currState.getName().equals(targetState.getName())) {
+				break;
+			}
+			
+			statesTraversed.add(currState);
+			
+			currState = currState.getParentState();
+		}
+		return statesTraversed;
 	}
 }
--- src/test/java/org/javaki/StateTraversalTest.java
+++ src/test/java/org/javaki/StateTraversalTest.java
@@ -12,6 +12,10 @@ public class StateTraversalTest {
 	
 	@Before
 	public void setup() {
+		initializeStatechart();
+	}
+	
+	private void initializeStatechart() {
 		State rootState = new State(null, "ROOT");
 		//First level
 		State a = rootState.addState(new State("A"));
@@ -27,7 +31,7 @@ public class StateTraversalTest {
 		//Third Level
 		State e = d.addState(new State("E"));
 		State g = f.addState(new State("G"));
-		State j = i.addState(new State("I"));
+		State j = i.addState(new State("J"));
 		State k = i.addState(new State("K"));
 		State n = m.addState(new State("N"));
 		
@@ -41,6 +45,7 @@ public class StateTraversalTest {
 		q = o.addState(new State("Q"));
 		
 		statechart = new Statechart("testchart", rootState);
+		statechart.initializeStatechart();
 	}
 	
 	@Test
@@ -100,5 +105,127 @@ public class StateTraversalTest {
 		
 		actual = q.getParentState("X");
 		Assert.assertNull("X is not a parent of Q", actual);
+	}
+	
+	@Test
+	public void verify_that_the_right_create_states_is_created_when_moving_to_substate() {
+		statechart.gotoState("G");
+		Assert.assertTrue("Forventer en ikke tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 3 elementer i createStateChain",  new Integer(3), new Integer(statechart.createStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i createStateChain er A", "A", statechart.createStateChain.get(0).getName());
+		Assert.assertEquals("Forventer at 2 element i createStateChain er F", "F", statechart.createStateChain.get(1).getName());
+		Assert.assertEquals("Forventer at 3 element i createStateChain er G", "G", statechart.createStateChain.get(2).getName());
+		
+		statechart.gotoState("H");
+		Assert.assertTrue("Forventer en ikke tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i createStateChain", new Integer(1), new Integer(statechart.createStateChain.size()) );
+		Assert.assertEquals("Forventer at 1 element i createStateChain er H", "H", statechart.createStateChain.get(0).getName());
+		
+		initializeStatechart();
+		statechart.gotoState("C");
+		Assert.assertTrue("Forventer en ikke tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i createStateChain", new Integer(1), new Integer(statechart.createStateChain.size()) );
+		Assert.assertEquals("Forventer at 1 element i createStateChain er C", "C", statechart.createStateChain.get(0).getName());
+		
+		statechart.gotoState("M");
+		Assert.assertTrue("Forventer en ikke tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i createStateChain", new Integer(1), new Integer(statechart.createStateChain.size()) );
+		Assert.assertEquals("Forventer at 1 element i createStateChain er M", "M", statechart.createStateChain.get(0).getName());
+		
+		statechart.gotoState("N");
+		Assert.assertTrue("Forventer en ikke tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i createStateChain", new Integer(1), new Integer(statechart.createStateChain.size()) );
+		Assert.assertEquals("Forventer at 1 element i createStateChain er N", "N", statechart.createStateChain.get(0).getName());
+		
+		statechart.gotoState("O");
+		Assert.assertTrue("Forventer en ikke tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i createStateChain", new Integer(1), new Integer(statechart.createStateChain.size()) );
+		Assert.assertEquals("Forventer at 1 element i createStateChain er O", "O", statechart.createStateChain.get(0).getName());
+		
+		statechart.gotoState("Q");
+		Assert.assertTrue("Forventer en ikke tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i createStateChain", new Integer(1), new Integer(statechart.createStateChain.size()) );
+		Assert.assertEquals("Forventer at 1 element i createStateChain er Q", "Q", statechart.createStateChain.get(0).getName());
+	}
+	
+	@Test
+	public void verify_that_the_right_destroy_states_is_creates_when_moving_to_parent_state() {
+		statechart.gotoState("Q");
+		
+		statechart.gotoState("M");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 3 elementer i destroyStateChain",  new Integer(3), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er Q", "Q", statechart.destroyStateChain.get(0).getName());
+		Assert.assertEquals("Forventer at 2 element i destroyStateChain er O", "O", statechart.destroyStateChain.get(1).getName());
+		Assert.assertEquals("Forventer at 3 element i destroyStateChain er N", "N", statechart.destroyStateChain.get(2).getName());
+		
+		initializeStatechart();
+		statechart.gotoState("Q");
+		Assert.assertTrue("Forventer en ikke-tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 5 elementer i createStateChain",  new Integer(5), new Integer(statechart.createStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i createStateChain er C", "C", statechart.createStateChain.get(0).getName());
+		Assert.assertEquals("Forventer at 2 element i createStateChain er M", "M", statechart.createStateChain.get(1).getName());
+		Assert.assertEquals("Forventer at 3 element i createStateChain er N", "N", statechart.createStateChain.get(2).getName());
+		Assert.assertEquals("Forventer at 4 element i createStateChain er O", "O", statechart.createStateChain.get(3).getName());
+		Assert.assertEquals("Forventer at 5 element i createStateChain er Q", "Q", statechart.createStateChain.get(4).getName());
+		
+		statechart.gotoState("O");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i destroyStateChain",  new Integer(1), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er Q", "Q", statechart.destroyStateChain.get(0).getName());
+		
+		statechart.gotoState("N");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i destroyStateChain",  new Integer(1), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er O", "O", statechart.destroyStateChain.get(0).getName());
+		
+		statechart.gotoState("M");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i destroyStateChain",  new Integer(1), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er N", "N", statechart.destroyStateChain.get(0).getName());
+		
+		statechart.gotoState("C");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i destroyStateChain",  new Integer(1), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er M", "M", statechart.destroyStateChain.get(0).getName());
+	}
+	
+	@Test
+	public void verify_that_the_right_destroy_and_create_states_are_created_when_switching_branches_in_the_tree() {
+		statechart.gotoState("Q");
+		
+		statechart.gotoState("P");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 2 elementer i destroyStateChain",  new Integer(2), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er Q", "Q", statechart.destroyStateChain.get(0).getName());
+		Assert.assertEquals("Forventer at 2 element i destroyStateChain er O", "O", statechart.destroyStateChain.get(1).getName());
+		
+		Assert.assertTrue("Forventer en ikke-tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i createStateChain",  new Integer(1), new Integer(statechart.createStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i createStateChain er P", "P", statechart.createStateChain.get(0).getName());
+		
+		statechart.gotoState("K");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 4 elementer i destroyStateChain",  new Integer(4), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er P", "P", statechart.destroyStateChain.get(0).getName());
+		Assert.assertEquals("Forventer at 2 element i destroyStateChain er N", "N", statechart.destroyStateChain.get(1).getName());
+		Assert.assertEquals("Forventer at 3 element i destroyStateChain er M", "M", statechart.destroyStateChain.get(2).getName());
+		Assert.assertEquals("Forventer at 4 element i destroyStateChain er C", "C", statechart.destroyStateChain.get(3).getName());
+		
+		Assert.assertTrue("Forventer en ikke-tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 3 elementer i createStateChain",  new Integer(3), new Integer(statechart.createStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i createStateChain er B", "B", statechart.createStateChain.get(0).getName());
+		Assert.assertEquals("Forventer at 2 element i createStateChain er I", "I", statechart.createStateChain.get(1).getName());
+		Assert.assertEquals("Forventer at 3 element i createStateChain er K", "K", statechart.createStateChain.get(2).getName());
+		
+		statechart.gotoState("L");
+		Assert.assertTrue("Forventer en ikke-tom destroyStateChain", statechart.destroyStateChain.size() > 0);
+		Assert.assertEquals("Forventer 1 elementer i destroyStateChain",  new Integer(1), new Integer(statechart.destroyStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i destroyStateChain er K", "K", statechart.destroyStateChain.get(0).getName());
+		
+		Assert.assertTrue("Forventer en ikke-tom createStateChain", statechart.createStateChain.size() > 0);
+		Assert.assertEquals("Forventer 2 elementer i createStateChain",  new Integer(2), new Integer(statechart.createStateChain.size()));
+		Assert.assertEquals("Forventer at 1 element i createStateChain er J", "J", statechart.createStateChain.get(0).getName());
+		Assert.assertEquals("Forventer at 2 element i createStateChain er L", "L", statechart.createStateChain.get(1).getName());
 	}
 }
